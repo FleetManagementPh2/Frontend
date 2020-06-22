03b89618c8b8de99ade28187fa10301d9d92d7da
MalavVakharia-11 <67009133+MalavVakharia-11@users.noreply.github.com>
2020-06-22 19:30:51 +0530

    First Commit With Proper Files

---
+++ FleetManagementMaster/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './Component/login/login.component';
+import { RegistrationComponent } from './Component/registration/registration.component';
+import { ResetPasswordComponent } from './Component/reset-password/reset-password.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    ResetPasswordComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
